39c45bba5e54199d6702e045796c3bfdf44f389b
ReisenCW <814568436@qq.com>
2024-11-28 10:51:08 +0800

    Small Update: 把tile的y坐标置0, 在Games/Source目录下的Tj_Stardew_Valley.Build.cs文件中加入Paper2D与PaperZD模块后rebuild了解决方案,以便在C++中使用Paper2D与PaperZD插件的功能

--- Source/Tj_Stardew_Valley/Tj_Stardew_Valley.Build.cs
+++ Source/Tj_Stardew_Valley/Tj_Stardew_Valley.Build.cs
@@ -8,7 +8,8 @@ public class Tj_Stardew_Valley : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", 
+			"Paper2D", "PaperZD" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
