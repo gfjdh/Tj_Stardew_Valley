d70499a6c98f805f378ca9c39424b862e1d5bbde
Yelo-Pandora <yp_0721@outlook.com>
2024-12-19 01:27:09 +0800

    Revert "现在作物用锄头挖了之后可以掉东西"
    This reverts commit 816031c6c380e7670a591219a899ff9def9ee980.

--- Source/Tj_Stardew_Valley/Public/Crop.h
+++ Source/Tj_Stardew_Valley/Public/Crop.h
@@ -26,37 +26,6 @@ public:
 	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
 	UPaperSpriteComponent* TreeSprite;
 
-<<<<<<< Updated upstream
-=======
-	//决定作物的当前成长状态，并以此来切换外观，和决定收获物
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
-	int status=0;
-
-	//作物的当前成熟度，以此决定作物当前的成长状态
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
-	double Maturity=0;
-
-	//作物各阶段对应的sprite
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	UPaperSprite* CropStageSprite1;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	UPaperSprite* CropStageSprite2;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	UPaperSprite* CropStageSprite3;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	UPaperSprite* CropStageSprite4;
-
-	//作物被收获后生成的两个产物，分别是种子和产品
-	UPROPERTY(EditDefaultsOnly, Category = "Product")
-	TSubclassOf<ACollectableEntity> ProductActor1;
-
-	UPROPERTY(EditDefaultsOnly, Category = "Product")
-	TSubclassOf<ACollectableEntity> ProductActor2;
-
->>>>>>> Stashed changes
 	// Sets default values for this actor's properties
 	ACrop();
 
@@ -71,19 +40,4 @@ public:
 	UFUNCTION()
 	void OverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
 		int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
-<<<<<<< Updated upstream
-=======
-
-	//改变当前作物状态的函数
-	UFUNCTION()
-	void JudgeMaturity();
-
-	//改变当前作物外观
-	UFUNCTION()
-	void SwitchSprite();
-
-	//生成产品
-	UFUNCTION()
-	void SpawnProducts();
->>>>>>> Stashed changes
 };
