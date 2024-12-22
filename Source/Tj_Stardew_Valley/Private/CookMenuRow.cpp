d929cea4a5cfe18cf8d154c0884a052223bfe63f
ReisenCW <814568436@qq.com>
2024-12-22 17:08:49 +0800

    cook经验不够不能用

--- Source/Tj_Stardew_Valley/Private/CookMenuRow.cpp
+++ Source/Tj_Stardew_Valley/Private/CookMenuRow.cpp
@@ -14,12 +14,20 @@ void UCookMenuRow::OnCookButtonClicked()
 	//get inventory
 	AMyPaperZDCharacter* Player = Cast<AMyPaperZDCharacter>(UGameplayStatics::GetPlayerPawn(GetWorld(), 0));
 	if (Player) {
+		//检查经验是否足够
+		if (Player->PlayerSkill->Cooking->Exp < DesiredExp) {
+			GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("You don't have enough EXP!"));
+			return;
+		}
 		UInventory* Inventory = Player->PlayerInventory;
 		if (Inventory) {
 			//检查材料是否足够
 			if (Inventory->HasItem(MaterialIndexA) && Inventory->HasItem(MaterialIndexB)) {
 				Inventory->RemoveItem(MaterialIndexA, 1);
 				Inventory->RemoveItem(MaterialIndexB, 1);
+				//加经验
+				GEngine->AddOnScreenDebugMessage(-1, 5.f, FColor::Red, TEXT("Add EXP 10!"));
+				Player->PlayerSkill->SkillStrucUpdate(SkillType::Cooking, 10);
 				//在玩家脚底生成食物
 				FVector SpawnLocation = Player->GetActorLocation();
 				GetWorld()->SpawnActor<ACollectableEntity>(CollectableEntityClass, SpawnLocation, FRotator::ZeroRotator);
