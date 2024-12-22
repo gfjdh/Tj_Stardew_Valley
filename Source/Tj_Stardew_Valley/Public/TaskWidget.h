d5cf15033bcc9cda6593c4b841b1a92942a0bb2a
ReisenCW <814568436@qq.com>
2024-12-22 17:43:09 +0800

    打开与关闭任务列表

--- Source/Tj_Stardew_Valley/Public/TaskWidget.h
+++ Source/Tj_Stardew_Valley/Public/TaskWidget.h
@@ -4,6 +4,7 @@
 #include "Blueprint/UserWidget.h"
 
 #include "Components/image.h"
+#include "Components/Button.h"
 #include "Components/TextBlock.h"
 #include "Components/GridPanel.h"
 
@@ -14,11 +15,19 @@ class TJ_STARDEW_VALLEY_API UTaskWidget : public UUserWidget
 {
 	GENERATED_BODY()
 public:
+	virtual void NativeConstruct() override;
+public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (BindWidget))
 	UGridPanel* TaskGridPanel;
 	
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (BindWidget))
 	UTextBlock* TitleText;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (BindWidget))
+	UButton* CloseButton;
+
 	bool IsOpened = false;
+
+	UFUNCTION()
+	void OnCloseButtonClicked();
 };
